cb7ec577f3bce7e88e13d35e8f57933c9b3f26a2
RooTurturro <rooturturro65@gmail.com>
2023-01-03 15:00:53 -0300

    models complete

---
+++ api/database/migrations/20230103171402-create-order.js
@@ -0,0 +1,34 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      delivered: {
+        type: Sequelize.BOOLEAN
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ api/database/models/detail.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Detail extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Detail.init({
+    subtotal: DataTypes.INTEGER,
+    specification: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Detail',
+  });
+  return Detail;
+};
