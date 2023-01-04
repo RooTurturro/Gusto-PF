15c3010c2cfe29ae828616ea093d13cf3c2aa2c2
Eduardo Toro <cris122493@gmail.com>
2023-01-03 20:22:21 -0500

    Back-3 table relation

--- api/database/models/user.js
+++ api/database/models/user.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Order, {
+        foreignKey: "userId",
+      });
     }
   }
   User.init(
