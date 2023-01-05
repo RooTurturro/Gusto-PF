578c3d12e643064db89a91c2224767888d93ba40
Emi-re997 <emilianore997@gmail.com>
2023-01-05 16:34:44 -0300

    Back-04 CRUD CATEGORY AND CRUD TOPPING

--- api/src/routing/routes/categoryRouter.js
+++ api/src/routing/routes/categoryRouter.js
@@ -1,21 +1,43 @@
 const { Router } = require("express");
 const {Product, Category, Toppings} = require('../../database/models/index')
-const { getInfoDB } = require("../controllers/productsControllers")
+const { getInfoDB , getInfoCategory} = require("../controllers/productsControllers")
 
 
 const categoryRouter = Router();
 
-categoryRouter.get("/", (req, res) =>{
-    res.send("Estamos en get de category")
+categoryRouter.get("/", async (req, res) =>{
+    const allCategory = await getInfoCategory() 
+    try{
+    res.status(200).send(allCategory)
+}
+catch {res.status(400).send(error)}
 })
 
-categoryRouter.post("/", (req, res) =>{
-    res.send("Estamos en post de category")
+categoryRouter.post("/", async (req, res)=>{
+    try {
+        const {name, description, price, image, state  } = req.body;
+        const newCategory = await Category.create({name, description, price, image, state})
+        res.status(201).send(newCategory);
+    } 
+    catch (error) {
+        res.status(400).send(error);
+    }
 })
 
-categoryRouter.delete("/", (req, res) =>{
-    res.send("Estamos en delete de category")
-})
+categoryRouter.delete("/", async (req, res) =>{
+    const { id } = req.params;
+    try {
+      const categoryDelete = await Category.findByPk(id);
+      if (!categoryDelete) {
+        res.status(400).send("No existe la Categoria que deseas eliminar");
+      } else {
+        categoryDelete.destroy();
+        return res.status(200).send("Categoria eliminado correctamente");
+      }
+    } catch (error) {
+      res.status(400).json({ error: error.message }, "Entré al error de delete");
+    }
+  });
 
 categoryRouter.put("/", (req, res) =>{
     res.send("Estamos en put de category")
