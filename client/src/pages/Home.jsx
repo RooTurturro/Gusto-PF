03ca34103ef597c6f88c00c006a796df76e11e17
franco <francoadrian.coll2012@gmail.com>
2023-01-06 00:21:50 -0300

    Home change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getProducts } from '../redux/actions'
-import Card from '../Components/Card/Card'
+import React from 'react'
 
 const Home = () => {
-  const dispatch = useDispatch()
-  const allProducts = useSelector((state) => state.products)
-
-  useEffect(() => {
-    dispatch(getProducts())
-  }, [dispatch])
-  console.log(allProducts)
+  
 
   return (
     <div>
-
-      {
-        allProducts.length > 0 ?
-          allProducts.map((el) => {
-            return (
-              <Card
-                key={el.id}
-                name={el.name}
-                description={el.description}
-                price={el.price}
-                image={el.image}
-              />
-            )
-
-          }) : <div>nada</div>}
-
+      home
     </div>
   )
 }
