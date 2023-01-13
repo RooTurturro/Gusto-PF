d2200b1742ba7f7526fecc3ee8a4e042f717faf2
Emi-re997 <emilianore997@gmail.com>
2023-01-13 17:13:59 -0300

    Front 27 , routing SendGrid auto

--- api/src/routing/routes/sendGridRouter.js
+++ api/src/routing/routes/sendGridRouter.js
@@ -22,4 +22,73 @@ sendGrid.post("/", async (req,res)=>{
   res.status(201).send({success : true})
 });
 
+sendGrid.post("/checkout", async (req,res)=>{
+
+  /*const detail = await axios.get("https://localhost/3001/purchase")
+  const sendDetail = {
+    compraId: detail.id,
+    productName: detail.products.name,
+    productPrice: detail.products.price,
+    product2: detail.specification,
+    product3: detail.total
+  }*/
+  const { to , name } = req.body ; 
+ 
+   const msg = {
+     to,
+     from: `emi-re11@hotmail.com`,
+     subject: "Su pedido esta siendo preparado",
+     text:"Gracias por su compra",
+     html: `<h1>Preparando pedido</h1><strong>Muchas gracias por su compra ${name} esperamos volver a verlo pronto, Los detalles de su pedido son ${sendDetail},</strong><strong>Gusto</strong>`,
+    
+   }
+   try{
+     await sgMail.send(msg)
+   }
+   catch(err){
+     return res.status(err.code).send(err.message)
+   }
+   res.status(201).send({success : true})
+ });
+
+ sendGrid.post("/login", async (req,res)=>{
+  const { to , name } = req.body ; 
+ 
+   const msg = {
+     to,
+     from: `emi-re11@hotmail.com`,
+     subject: "Su cuenta a sido creada :)",
+     text:"Cuenta creada con exito ",
+     html: `<h1>Cuenta registrada con exito</h1><strong>Su cuenta a sido creada con exito , Esperamos pronto su pedido</strong>`,
+    
+   }
+   try{
+     await sgMail.send(msg)
+   }
+   catch(err){
+     return res.status(err.code).send(err.message)
+   }
+   res.status(201).send({success : true})
+ });
+
+ sendGrid.post("/updateAccount", async (req,res)=>{
+  const { to , name } = req.body ; 
+ 
+   const msg = {
+     to,
+     from: `emi-re11@hotmail.com`,
+     subject: "Su cuenta a sido modificada:)",
+     text:"Cuenta su cuenta a sido modificada",
+     html: `<h1>Su cuenta se modifico con exito ! </h1><strong>Hola ${name}.Su cuenta a sido modificada con exito</strong>`,
+    
+   }
+   try{
+     await sgMail.send(msg)
+   }
+   catch(err){
+     return res.status(err.code).send(err.message)
+   }
+   res.status(201).send({success : true})
+ });
+ 
 module.exports = sendGrid;
