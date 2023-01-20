a51b55cb238e636cc17f3efb04b063b2b40abf69
Eduardo Toro <cris122493@gmail.com>
2023-01-20 12:14:31 -0500

    Back-52-DbugOne

--- api/src/routing/routes/purchaseRouter.js
+++ api/src/routing/routes/purchaseRouter.js
@@ -31,20 +31,21 @@ purchaseRouter.get("/:id", async (req, res) => {
 //create purchase
 
 
-purchaseRouter.put("/state/:id", async (req,res)=>{
-    try {
-        const { id } = req.params;
-        const { state} = req.body;
-        const findProduct = await Purchase.findByPk(id)
-        await findProduct.update(
-            { state},
-            { where: { id: id } }
-            );
-        res.status(200).send("Estado modificado con exito");
-        } 
-        catch (error) {
-        console.log("entre al error del put", error);
-    }
+purchaseRouter.put("/state/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { state } = req.body;
+    const findProduct = await Purchase.findByPk(id)
+    await findProduct.update(
+      { state },
+      { where: { id: id } }
+    );
+    res.status(200).send("Estado modificado con exito");
+  }
+  catch (error) {
+    console.log("entre al error del put", error);
+  }
+});
 
 purchaseRouter.post("/", async (req, res) => {
   try {
@@ -69,6 +70,7 @@ purchaseRouter.post("/", async (req, res) => {
       state,
     });
     res.status(201).json(newPurchase);
+
   } catch (error) {
     res.status(400).send(error);
     console.log(error);
@@ -85,8 +87,10 @@ purchaseRouter.put("/specifications/:id", async (req, res) => {
       { where: { id: id } }
     );
     res.status(200).send("Estado modificado con exito");
+
   } catch (error) {
     console.log(error);
   }
 });
-module.exports = purchaseRouter;
+  
+  module.exports = purchaseRouter;
