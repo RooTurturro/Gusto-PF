91055376038bb825703f6907bb091d833d8aec02
Franco Coll <91277981+francocoll@users.noreply.github.com>
2023-01-22 04:25:47 -0300

    mas cambios jaja

--- client/src/Components/Perfil/Perfil.jsx
+++ client/src/Components/Perfil/Perfil.jsx
@@ -7,98 +7,50 @@ import LogoutButton from '../Login/LogoutButton';
 
 
 const Perfil = () => {
-
     const dispatch = useDispatch();
     const usuario = useSelector((state) => state.user);
-    const log = window.localStorage.getItem("isLogIn");
-
-    console.log('Estado Global ' + usuario)
-
-
     const email = window.localStorage.getItem('userEmail')
-
-
-
     const picture = window.localStorage.getItem('userPicture')
 
-    console.log('este es localstorage antes del use ' + email)
-    const variablePrueba = 'info fuera del use'
-    console.log(log)
-    console.log(email)
-
     useEffect(() => {
-
-
-        console.log('Porque no haces dispatch')
-        console.log(variablePrueba)
-
         dispatch(userProfile(email))
-
-
-
-
     }, [dispatch])
-
-    console.log('estado global' + usuario.name)
-
-
-
-
 
 
     return (
-
-
         <div>
             <div className={styles.container}>
                 <div className={styles.bloque}>
                     <div className={styles.left}>
-
                         <div>
                             <img src={picture} className={styles.imagen} alt="imagen de usuario" />
                         </div>
-
                         <div>
                             <h3 className={styles.text2}>{usuario.name}</h3>
                         </div>
-
                     </div>
-
                     <div className={styles.right}>
                         <div className={styles.info}>
-
                             <div>
-
                                 <h3 className={styles.titulo}>Correo</h3>
                                 <p className={styles.text}>{usuario.email}</p>
-
                             </div>
-
                             <div>
                                 <h3 className={styles.titulo}>Teléfono</h3>
                                 <p className={styles.text}>{usuario.phone}</p>
-
                             </div>
-
                             <div>
                                 <h3 className={styles.titulo}>Dirección</h3>
                                 <p className={styles.text}>{usuario.address}</p>
                             </div>
-
                         </div>
-
-
                         <div className={styles.botones}>
-
-
                             <LogoutButton />
                             <Link to={'/editarperfilusuario'}>
                                 <button className={styles.button}>
                                     <span>Editar Perfil</span>
                                 </button>
                             </Link>
-
-
                         </div>
                     </div>
                 </div>
