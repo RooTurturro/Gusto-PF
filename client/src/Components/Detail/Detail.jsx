a03968e37a7af6014912831f80110a8eb5ab9fe3
Franco Coll <91277981+francocoll@users.noreply.github.com>
2023-01-26 11:27:09 -0300

    EDIT

--- client/src/Components/Detail/Detail.jsx
+++ client/src/Components/Detail/Detail.jsx
@@ -10,12 +10,15 @@ import arrow from '../../assets/arrow.png'
 import RatingProducts from '../RatingProducts/RatingProducts'
 import videomaqueta from '../../assets/videoMaqueta.png'
 import hottestImage from '../../assets/hottestImage.png'
+import Edit from '../Edit'
+
 
 const Detail = () => {
 	const navigate = useNavigate();
 	const { id } = useParams();
 	const product = useSelector((state) => state.productDetail);
 	const dispatch = useDispatch();
+	const user = useSelector((state) => state.user)
 
 	useEffect(() => {
 		dispatch(getProductsDetail(id));
@@ -33,7 +36,7 @@ const Detail = () => {
 
 	return (
 		<div className={styles.detail}>
-			
+
 			<div className={styles.detailContainer}>
 				<div className={styles.detailInfo}>
 					<div>
@@ -47,6 +50,7 @@ const Detail = () => {
 					</div>
 					<div style={{ width: '40rem', display: 'flex', justifyContent: 'space-between' }}>
 						<button className={styles.arrowContainer} onClick={() => navigate(-1)} ><img className={styles.arrow} src={arrow} alt='nada' />VOLVER</button>
+
 						<div style={{ display: 'flex', width: '50rem', alignItems: 'flex-end', marginLeft: '30rem' }}>
 							<div style={{ display: 'flex', flexDirection: 'column' }}>
 								<img src={arrow} className={styles.videoArrow} alt="flecha" />
@@ -76,6 +80,11 @@ const Detail = () => {
 					<img className={styles.detailImage} src={product.image} alt='foto' />
 				</div>
 			</div>
+			{user.isAdmin ? (
+				<div style={{ display: 'flex', justifyContent: 'center' }}>
+					<Edit productDetail={product} />
+				</div>
+			) : null}
 		</div>
 	);
 };
